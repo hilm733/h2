16873f2250f259b527ca551e033be728cbaf5c18
master <h733116738@gmail.com>
2025-09-20 17:37:33 +0300

    add themeNotifier in main and include a library

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'dark_light.dart';
 import 'login.dart';
 void main() {
   runApp(A());
@@ -8,7 +10,29 @@ class A extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+
     return MaterialApp(
+      // --------------
+      debugShowCheckedModeBanner: false,
+      themeMode: themeNotifier.themeMode,
+      theme: ThemeData(
+        // تنسيق الوضع النهاري
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.blue.shade500,
+        brightness: Brightness.light,
+
+        // ... أضف المزيد من الألوان والتنسيقات للوضع النهاري
+      ),
+      darkTheme: ThemeData(
+        // تنسيق الوضع الليلي
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.black87,
+        brightness: Brightness.dark,
+        // ... أضف المزيد من الألوان والتنسيقات للوضع الليلي
+      ),
+
+
       home:Log() ,
       // --------------
     );
