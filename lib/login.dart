271173de98d5656e6689344b0bf9a2787ae4a2f7
master <h733116738@gmail.com>
2025-09-20 16:37:04 +0300

    add  icon person and textfield name in login interface

--- lib/login.dart
+++ lib/login.dart
@@ -23,7 +23,48 @@ class Log extends StatelessWidget {
           ],
         ),
         // drawer:DDrawer(context),
+        body: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Center(
+              child: Container(
 
+                child: SingleChildScrollView(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    spacing: 20,
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        child: Icon(
+                          Icons.person_outline,
+                          size: 100,
+                        ),
+                      ),
+                      TextFormField(
+                        // textDirection:TextDirecton.rtl,
+                        decoration: InputDecoration(
+                          floatingLabelAlignment: FloatingLabelAlignment.center,
+                          // floatingLabelBehavior: FloatingLabelBehavior.always,
+                          prefixIcon: Icon(Icons.person),
+                          hintText: "Enter user_name",
+                          labelText: " user_name",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+
+
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
